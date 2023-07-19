dfe574f1dc4a2b31a54cde7a33aa3658d80dc39a
Omar González <omar_gonza8@live.com>
2023-07-19 14:43:28 -0500

    Estructura Examen

---
+++ src/main/java/ec/edu/espe/arquitectura/examen_gonzalez/ExamenGonzalezApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen_gonzalez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenGonzalezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenGonzalezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/examen_gonzalez/ExamenGonzalezApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen_gonzalez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenGonzalezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
